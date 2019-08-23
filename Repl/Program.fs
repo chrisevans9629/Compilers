bb5e871476e235a8720ef4f3d41a1a9862357b12
Chris Evans <chrisevans9629@gmail.com>
2019-08-22 20:25:35 -0500

    added f# repl program

---
+++ Repl/Program.fs
@@ -0,0 +1,19 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Tokenizer
+let rec runRepl eval =
+    let line = Console.ReadLine()
+    if line = "exit" then ()
+    else
+        let result = eval line
+        Console.WriteLine(result :> Object);
+        runRepl eval
+
+    
+
+[<EntryPoint>]
+let main argv =
+    printfn "Enter an expression"
+    runRepl (fun s -> tokenize s)
+    0 // return an integer exit code
---
+++ Repl/Tokenizer.fs
@@ -0,0 +1,23 @@
+﻿module Tokenizer
+open System
+type Tokens = Integer of int | Plus of string | Error of string
+
+
+
+let rec getDigit index (str:string) (value:string) =
+    if index > str.Length-1 then
+        Some (Integer (Int32.Parse(value)))
+    else
+    let cVal = str.[index]
+    if Char.IsDigit(cVal) then
+        let nvalue = value + cVal.ToString()
+        getDigit (index + 1) str nvalue
+    else if value <> "" then Some(Integer (Int32.Parse(value)))
+    else None
+
+
+
+let tokenize (str:string) =
+    [getDigit 0 str ""]
+   
+    
